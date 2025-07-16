98d5809fd9eed81611d0f32afdb560ccb01daa85
Birat <thapabirat95@gmail.com>
2025-07-16 21:39:42 +0545

    text

--- src/database/model/noticesModel.js
+++ src/database/model/noticesModel.js
@@ -11,7 +11,7 @@ module.exports = (sequelize , DataTypes ) => {
         },
 
         description:{
-            type:DataTypes.STRING,
+            type:DataTypes.TEXT,
             allowNull:false
         },
 
